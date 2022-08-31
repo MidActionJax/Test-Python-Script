01970e5e60e06349220c36c3eec4e40fc20b9f70
MidActionJax <jaxonjdoolittle@gmail.com>
2022-08-31 08:31:00 -0700

    Simple python test script

---
+++ Test Python script.py
@@ -0,0 +1,18 @@
+# <Jaxon>, <27749>, <8-23-2022>
+#Jaxon, JAX2146698, CIS156 13401
+
+
+#Hello World
+print("Hello World!")
+
+#Information
+name = (input("Hello, please enter your name:"))
+food = (input("What would you like to eat?:"))
+cost = str(input("How much does this item cost?:")) 
+cost = int(cost)
+tip = str(5)
+tip = int("5")
+total = str(cost + tip)
+
+#Final Message
+print("Hello, " + name + ". You would like, " + food + ". Total: " + total)
